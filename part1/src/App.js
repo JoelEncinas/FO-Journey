805fcd24735650696c092290c21f52db89a518f8
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-09 14:16:49 +0200

    render data in app component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,16 @@
 function App() {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
+
   return (
-    <p>Hello World</p>
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
   );
 }
 
