fb973bf40fc2e9a24ee7c122a473b30cbfcd12d6
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-09 16:28:49 +0200

    pass object as props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,13 @@
 function App() {
+  const friends = [
+    { name: "Peter", age: 4 },
+    { name: "Maya", age: 10 },
+  ];
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Joel" />
+      <Hello friends={friends} />
     </div>
   );
 }
@@ -12,7 +17,7 @@ export default App;
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.friends[0].name} {props.friends[1].age}</p>
     </div>
   );
 };
