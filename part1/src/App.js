245b87d98c8635c3479e21412dadcceb75e145b7
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-09 16:17:29 +0200

    multiple components and props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,18 @@
 function App() {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
-
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name="Joel" />
     </div>
   );
 }
 
 export default App;
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  );
+};
