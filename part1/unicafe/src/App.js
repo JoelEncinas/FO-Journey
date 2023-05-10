77807284fcbad33fc4d8343e3cd8fc70c2e98683
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-10 02:09:04 +0200

    unicafe 1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,35 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const setGoodValue = (newValue) => {
+    setGood(newValue);
+  };
+
+  const setNeutralValue = (newValue) => {
+    setNeutral(newValue);
+  };
+
+  const setBadValue = (newValue) => {
+    setBad(newValue);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGoodValue(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutralValue(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBadValue(bad + 1)} text="Bad" />
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 export default App;
