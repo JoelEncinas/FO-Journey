deaa29fcde2f54a1ae3cddbb8aa407817c60050b
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-10 02:36:10 +0200

    1.9 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -35,17 +35,26 @@ const Button = (props) => (
 );
 
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
-      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
-    </div>
-  );
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
+        <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+      </div>
+    );
+  }
 };
 
 export default App;
