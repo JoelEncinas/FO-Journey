cbf68e8b044e6d23202e26bcd62cd274f7ea17f8
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-12 00:14:18 +0200

    add functionality to toggle notes importance
    PUT req to the server to change if important

--- examples/notes/src/App.js
+++ examples/notes/src/App.js
@@ -17,6 +17,16 @@ const App = () => {
 
   useEffect(hook, []);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    });
+  };
+
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -49,7 +59,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
