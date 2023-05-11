7d61ca2d06d05a330c8757a1397c072b6e15e950
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-11 17:47:52 +0200

    2.6 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,14 +4,24 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
-  const handleNewNameChange = (event) => {
-    setNewName(event.target.value);
+  const addPerson = (e) => {
+    e.preventDefault();
+    const person = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(person));
+    setNewName("");
+  };
+
+  const handleNewNameChange = (e) => {
+    setNewName(e.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewNameChange} />
         </div>
