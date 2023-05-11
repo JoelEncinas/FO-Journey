92dd5b26b0af62f2ce6fe820adc07d85299eb80a
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-11 18:18:22 +0200

    added dummy data and id property to person

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -18,6 +21,7 @@ const App = () => {
       const person = {
         name: newName,
         number: newNumber,
+        id: persons.length + 1,
       };
       setPersons(persons.concat(person));
       setNewName("");
@@ -36,6 +40,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           <div>
