0bb471c731ca45a479656021878730b71ff606f7
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-11 17:43:13 +0200

    display persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,20 +4,34 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Persons persons={persons} />
     </div>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </>
   );
 };
 
