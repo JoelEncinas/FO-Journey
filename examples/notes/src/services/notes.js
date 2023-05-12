348ebef65b5c174f3d2b85b3c43fe62f102e4f56
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-12 02:09:56 +0200

    make more compact the export of note service

--- examples/notes/src/services/notes.js
+++ examples/notes/src/services/notes.js
@@ -18,7 +18,7 @@ const update = (id, newObject) => {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  getAll: getAll,
-  create: create,
-  update: update,
+  getAll,
+  create,
+  update,
 };
