e7d1b12ced021dce87ad5ac58290b4f5faa5f85e
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-12 02:03:16 +0200

    refactor axios calls into a service file

--- examples/notes/src/App.js
+++ examples/notes/src/App.js
@@ -1,29 +1,24 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
+  useEffect(() => {
+    noteService.getAll().then((response) => {
       setNotes(response.data);
     });
-  };
-
-  useEffect(hook, []);
+  }, []);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    noteService.update(id, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
     });
   };
 
@@ -34,8 +29,8 @@ const App = () => {
       important: Math.random() < 0.5,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(noteObject));
+    noteService.create(noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
       setNewNote("");
     });
   };
