799dabce4c682769526fae4eb12d8178c29e44f5
Joel Encinas <joelencinasmartin@gmail.com>
2023-05-24 19:00:06 +0200

    refactor error handling to middleware

--- examples/notes_backend/index.js
+++ examples/notes_backend/index.js
@@ -18,7 +18,7 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -27,9 +27,8 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((err) => {
-      console.log(err);
-      response.status(400).send({ error: "malformatted id" });
+    .catch((error) => {
+      next(error);
     });
 });
 
@@ -56,6 +55,18 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
