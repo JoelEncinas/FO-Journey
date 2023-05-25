94c4a73a126c046cad53e38b7f6d6d19a37aac8c
Joel Encinas <joelencinasmartin@gmail.com>
2023-05-25 19:24:55 +0200

    3.18 done
    delete hardcoded persons array, added catch blocks and make info endpoint work with db

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -15,33 +15,14 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  Person.find().then((persons) => {
-    res.status(200).json(persons);
-  });
+  Person.find()
+    .then((persons) => {
+      res.status(200).json(persons);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
@@ -71,10 +52,16 @@ const getDate = () => {
 };
 
 app.get("/info", (req, res) => {
-  res.status(200).send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${getDate()}</p>
-    `);
+  Person.find()
+    .then((persons) => {
+      res.status(200).send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${getDate()}</p>
+      `);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.post("/api/persons", (req, res) => {
